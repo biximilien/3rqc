815313cc9190430b171061a356d4dfb754116787
gh0st12 <couillardsamuel@gmail.com>
2020-04-27 20:29:51 -0400

    feat(frontend): Carousel
    Add Slick Carousel for images.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @images = Dir.chdir(Rails.root.join('app/assets/images')) do
+      Dir.glob("carousel/*.png")
+    end
   end
 
   def ca
   end
+
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'slick-carousel/slick/slick';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +16,30 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log("hey");
+
+  $('#images_carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    variableWidth: true,
+    slidesToShow: 1,
+    centerMode: true,
+    arrows: true,
+    nextArrow: '<i class="fas fa-chevron-right"></i>',
+    prevArrow: '<i class="fas fa-chevron-left"></i>',
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      }
+    }]
+  });
+
+
+});
