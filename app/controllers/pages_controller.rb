facc36bf3293cab3ec62b00b4c084ea97d16ef1f
gh0st12 <couillardsamuel@gmail.com>
2020-04-28 15:23:10 -0400

    feat(frontend): About Path
    Add About page with about us, mission and pictures.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
+
   def home
-    @images = Dir.chdir(Rails.root.join('app/assets/images')) do
-      Dir.glob("carousel/*.png")
-    end
+
   end
 
   def ca
   end
 
+  def about
+    @images = Dir.chdir(Rails.root.join('app/assets/images')) do
+      Dir.glob("carousel/*.png")
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'ca', to: 'pages#ca'
+  get 'about', to: 'pages#about'
+
 end
