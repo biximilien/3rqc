0c663e5a9a6baa9b323a18f6e25b7ec49a604983
gh0st12 <couillardsamuel@gmail.com>
2020-05-20 15:38:41 -0400

    fix(backend): Validation
    Proper error will now be display if user has entered date in past,
    or user has book an unavailable date.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
   validates :start_time,
     :uniqueness => {:message => "Cette plage horaire n'est pas disponible. Veuillez SVP en choisir une autre."}
 
+  validate :start_date_in_future, on: :create
+
+  def start_date_in_future
+    if start_time < Date.current
+      errors.add(:start_time, "Vous avez choisi une date antérieure. Veuillez SVP en choisir une autre.")
+    end
+  end
+
+
 end
