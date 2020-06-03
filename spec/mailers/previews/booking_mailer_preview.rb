a7f2d28420d7d3e96d6bff7e9f43c18b81fea7be
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-06-03 19:31:25 -0400

    feat: Make booking email nice
    Make the booking email nicer.

--- spec/mailers/previews/booking_mailer_preview.rb
+++ spec/mailers/previews/booking_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/booking_mailer
 class BookingMailerPreview < ActionMailer::Preview
-
+  def booking_email
+    BookingMailer.with(booking: Booking.first).booking_email
+  end
 end
